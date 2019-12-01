04378d1f3ac2955a8da56186f990bc4106efac2d
liyao <905309548@qq.com>
2019-12-01 17:36:02 +0800

    初始化项目

---
+++ src/main/java/com/briup/cms3/dao/ICategoryDao.java
@@ -0,0 +1,8 @@
+package com.briup.cms3.dao;
+
+
+import com.briup.cms3.bean.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ICategoryDao extends JpaRepository<Category,Integer> {
+}
---
+++ src/main/java/com/briup/cms3/dao/ICustomerDao.java
@@ -0,0 +1,7 @@
+package com.briup.cms3.dao;
+
+import com.briup.cms3.bean.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ICustomerDao extends JpaRepository<Customer,Integer> {
+}
